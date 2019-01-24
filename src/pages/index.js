9fb13124e7ac486eafff45b6caf5188861da92cb
Egrodo <egrodo1@gmail.com>
2019-01-24 15:52:49 -0500

    Blog section converted to staticquery

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { graphql } from "gatsby";
 import Canvas from "../components/Canvas";
 import HeaderSection from "../sections/HeaderSection";
 import ProjectSection from "../sections/ProjectSection";
@@ -9,37 +8,14 @@ import ContactSection from "../sections/ContactSection";
 import "normalize.css";
 import "./index.css";
 
-const indexPage = ({ data }) => {
-  const posts = data.allMarkdownRemark.edges.map(post => {
-    return post.node.frontmatter;
-  });
-
-  return (
-    <>
-      <Canvas />
-      <HeaderSection />
-      <ProjectSection />
-      <BlogSection posts={posts} />
-      <ContactSection />
-    </>
-  );
-};
-
-export const pageQuery = graphql`
-  query indexQuery {
-    allMarkdownRemark(limit: 3) {
-      edges {
-        node {
-          frontmatter {
-            title
-            excerpt
-            date(formatString: "MMM Do, YYYY")
-            path
-          }
-        }
-      }
-    }
-  }
-`;
+const indexPage = () => (
+  <>
+    <Canvas />
+    <HeaderSection />
+    <ProjectSection />
+    <BlogSection />
+    <ContactSection />
+  </>
+);
 
 export default indexPage;
--- src/sections/BlogSection.js
+++ src/sections/BlogSection.js
@@ -1,53 +1,79 @@
 import React from "react";
 import CSS from "./BlogSection.module.css";
-import { Link } from "gatsby";
+import { Link, StaticQuery } from "gatsby";
 
-const BlogSection = ({ posts }) => {
+const BlogSection = () => {
   return (
-    <section className={CSS.Blog} id="Blog">
-      <div className={CSS.sectionHeader}>
-        <a
-          href="{{ site.url }}/blog"
-          className={CSS.headerLink}
-          target="_blank"
-          rel="noopener noreferrer"
-          alt="Link to blog"
-          title="Click to go to blog
-      index"
-        >
-          <h1>Write-Ups</h1>
-        </a>
-      </div>
-      <div className={CSS.sectionArea}>
-        {posts.map((post, i) => (
-          <div className={CSS.blogPost} key={`${post.title}_${i}`}>
-            <div className={CSS.blogTitle}>
-              <div className={CSS.postTitle}>
-                <h2>
-                  <Link to={post.path}>{post.title}</Link>
-                </h2>
-              </div>
-              <div className={CSS.postDate}>
-                <span>{post.date}</span>
-              </div>
-            </div>
-
-            <p className={CSS.blogDesc}>{post.excerpt}</p>
-            <div className={CSS.blogLink}>
-              <Link
-                className={CSS.fancyLink}
-                to={post.path}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className={CSS.fancyLinkText}> Read More... </span>
-              </Link>
-            </div>
+    <StaticQuery
+      query={query}
+      render={({ allMarkdownRemark: { edges } }) => (
+        <section className={CSS.Blog} id="Blog">
+          <div className={CSS.sectionHeader}>
+            <a
+              href="{{ site.url }}/blog"
+              className={CSS.headerLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              alt="Link to blog"
+              title="Click to go to blog index"
+            >
+              <h1>Write-Ups</h1>
+            </a>
           </div>
-        ))}
-      </div>
-    </section>
+          <div className={CSS.sectionArea}>
+            {edges.map(({ node }, i) => (
+              <div
+                className={CSS.blogPost}
+                key={`${node.frontmatter.title}_${i}`}
+              >
+                <div className={CSS.blogTitle}>
+                  <div className={CSS.postTitle}>
+                    <h2>
+                      <Link to={node.frontmatter.path}>
+                        {node.frontmatter.title}
+                      </Link>
+                    </h2>
+                  </div>
+                  <div className={CSS.postDate}>
+                    <span>{node.frontmatter.date}</span>
+                  </div>
+                </div>
+
+                <p className={CSS.blogDesc}>{node.frontmatter.excerpt}</p>
+                <div className={CSS.blogLink}>
+                  <Link
+                    className={CSS.fancyLink}
+                    to={node.frontmatter.path}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span className={CSS.fancyLinkText}> Read More... </span>
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </div>
+        </section>
+      )}
+    />
   );
 };
+
+export const query = graphql`
+  query BlogQuery {
+    allMarkdownRemark(limit: 3) {
+      edges {
+        node {
+          frontmatter {
+            title
+            excerpt
+            date(formatString: "MMM Do, YYYY")
+            path
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default BlogSection;
