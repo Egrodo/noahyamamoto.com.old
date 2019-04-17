dc49dca0e6d0dd8bf592694534ce934e77fca174
Egrodo <egrodo1@gmail.com>
2019-04-17 15:23:24 -0400

    Trying to version image for update in Discord

--- src/components/HeadTag.js
+++ src/components/HeadTag.js
@@ -23,17 +23,17 @@ const HeadTag = ({ title, description, path }) => (
     <meta property="og:url" content={`https://noahyamamoto.com${path}`} />
     <meta property="og:site_name" content="Noah Yamamoto" />
     <meta property="og:description" content={description} />
-    <meta property="og:image" content="https://noahyamamoto.com/android-chrome-512x512.png" />
+    <meta property="og:image" content="https://noahyamamoto.com/android-chrome-512x512.png?v=2" />
 
     <meta name="twitter:title" content={title} />
     <meta name="twitter:description" content={description} />
-    <meta name="twitter:image" content="https://noahyamamoto.com/android-chrome-512x512.png" />
+    <meta name="twitter:image" content="https://noahyamamoto.com/android-chrome-512x512.png?v=2" />
     <meta name="twitter:card" content="summary" />
     <meta name="twitter:site" content="@egrodo" />
 
-    <link rel="apple-touch-icon" sizes="180x180" href="https://noahyamamoto.com/apple-touch-icon.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="https://noahyamamoto.com/favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="https://noahyamamoto.com/favicon-16x16.png" />
+    <link rel="apple-touch-icon" sizes="180x180" href="https://noahyamamoto.com/apple-touch-icon.png?v=2" />
+    <link rel="icon" type="image/png" sizes="32x32" href="https://noahyamamoto.com/favicon-32x32.png?v=2" />
+    <link rel="icon" type="image/png" sizes="16x16" href="https://noahyamamoto.com/favicon-16x16.png?v=2" />
     <link rel="manifest" href="https://noahyamamoto.com/site.webmanifest" />
     <link rel="mask-icon" href="https://noahyamamoto.com/safari-pinned-tab.svg" color="#b200f0" />
     <meta name="msapplication-TileColor" content="#b200f0" />
